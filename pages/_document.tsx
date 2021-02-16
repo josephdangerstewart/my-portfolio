9d7c88d35a7fd1739999f79e13c906fe2434a8ff
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-02-15 17:18:08 -0800

    Add server style sheets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,33 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
+	static async getInitialProps(ctx: DocumentContext) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
 	}
 
 	render() {
